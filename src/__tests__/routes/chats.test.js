23e9f7720db3e1af43f517ff579b107e6cdfdb9c
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-05-29 17:20:04 +0200

    Fix bugs

--- src/__tests__/routes/chats.test.js
+++ src/__tests__/routes/chats.test.js
@@ -1,27 +1,33 @@
 import request from 'supertest';
 import app from '../../app';
 import { urlPrefix } from '../../__mocks__/variables';
-import createUser from '../../middlewares/createUser';
 import * as statusCodes from '../../constants/statusCodes';
-
+import { User, Token } from '../../models';
 import { notExist, unauthorized } from '../../constants/responseMessages';
 
 const message = 'lorem ipsum dolor sit amet';
 
+let user;
+let token;
+let tokenData;
+const username = 'admin';
 describe('Chats', () => {
-    let body;
+
     beforeAll(async () => {
-        body = await createUser();
+        user = await User.findOne({ username });
+        tokenData = await Token.findOne({ _userId: user._id }).sort({
+            createdAt: -1,
+        });
+        token = `Bearer ${tokenData.token}`;
     });
+
     describe('Send a message to a user ', () => {
 
         test('Should send a new message', async () => {
-            const { token } = body;
-            const { username } = body.user.user;
 
             const res = await request(app)
                 .post(`${urlPrefix}/chats/${username}`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', token)
                 .send({ body: message });
             expect(res.status).toBe(statusCodes.OK);
             expect(res.body.chat.body).toBe(message);
@@ -29,11 +35,10 @@ describe('Chats', () => {
         });
 
         test('Should fail to send if the receiver does not exist', async () => {
-            const { token } = body;
 
             const res = await request(app)
                 .post(`${urlPrefix}/chats/not-exist`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', token)
                 .send({ body: message });
             expect(res.status).toBe(statusCodes.NOT_FOUND);
             expect(res.body.message).toBe(notExist('The user'));
@@ -52,21 +57,18 @@ describe('Chats', () => {
     describe('Get user chats ', () => {
 
         test('Should a user chats', async () => {
-            const { token } = body;
-            const { username } = body.user.user;
 
             const res = await request(app)
                 .get(`${urlPrefix}/chats/${username}`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', token)
             expect(res.status).toBe(statusCodes.OK);
         });
 
         test('Should fail to get the user chats if the user does not exist', async () => {
-            const { token } = body;
 
             const res = await request(app)
                 .get(`${urlPrefix}/chats/not-exist`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', token)
                 .send({ body: message });
             expect(res.status).toBe(statusCodes.NOT_FOUND);
             expect(res.body.message).toBe(notExist('The user'));
