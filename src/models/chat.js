20b8f318c2a42522935dfb904a954fb7cb1e8179
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-05-29 16:41:30 +0200

    Fix error

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,22 +1,22 @@
 import { Schema, model } from 'mongoose';
 
 const chatSchema = new Schema({
-    body: {
-        type: String,
-        required: true,
-    },
-    sender: {
-        type: Schema.Types.ObjectId, ref: 'User',
-        required: true,
-    },
-    receiver: {
-        type: Schema.Types.ObjectId, ref: 'User',
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    }
+  body: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: Schema.Types.ObjectId, ref: 'User',
+    required: true,
+  },
+  receiver: {
+    type: Schema.Types.ObjectId, ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  }
 });
 
 export default model('Chat', chatSchema);
